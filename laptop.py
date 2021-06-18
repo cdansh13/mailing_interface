1497d16f76ff2b8fba9ab807d1fefaa069e7e0e5
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-18 17:20:00 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Calculator")
 a=st.text_input("Your operator")
-b=st.text_input("Your first number")
-c=st.text_input("Your second number")
+b=st.text_input(int+"Your first number")
+c=st.text_input(int+"Your second number")
 d=st.button("Done")
 if d:
     if a=="+":
