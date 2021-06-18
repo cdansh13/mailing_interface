e8b98e058d54a6fb4699274fe4f983e8a963ef13
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-18 23:20:17 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -3,6 +3,14 @@ st.title("Calculator-Ansh Sharma")
 st.header("This calculator can do Addition")
 a=st.number_input("1st no.")
 b=st.number_input("2nd no.")
-c=st.button("Add")
-if c:
+addi=st.button("Addition")
+subt=st.button("Subtraction")
+mull=st.button("Multiplication")
+divide=st.button("Division")
+if addi:
    st.write(a+b)
+if subt:
+   st.write(a-b)
+if mull:
+   st.write(a*b)
+if    
