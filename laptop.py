5458eda3df8579bbbaf15df4bca4cd76ad9e4fd8
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-18 17:18:43 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -1,2 +1,20 @@
 import streamlit as st
-st.title("Hello")
+st.title("Calculator")
+a=st.text_input("Your operator")
+b=st.text_input("Your first number")
+c=st.text_input("Your second number")
+d=st.button("Done")
+if d:
+    if a=="+":
+      ad=b+c
+      st.write(ad)
+    if a=="-":
+      su=b-c
+      st.wrte(su)
+    if a=="*":
+      mull=b*c
+      st.write(mull)
+    if a=="/":
+      divide=b/c
+      st.write(divide)
+  
