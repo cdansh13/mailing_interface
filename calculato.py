ce9912b876c9ede5bb007a54bed3c24e3010c444
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-18 17:40:05 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Calculator-Ansh Sharma")
 st.header("This calculator can do Addition")
-a=st.text_input(int("1st number"))
-b=st.text_input(int("2nd number"))
+a=st.text_input(int("1"))
+b=st.text_input(int("2"))
 c=st.button("Add")
 if c:
    st.write(a+b)
