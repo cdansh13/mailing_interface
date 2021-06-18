d33e4002ca1a6ad9e802fa56e111901028581de6
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-18 17:23:10 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-.set_page_config(
-    page_title="Financial Planning Calculator")
+st.set_page_config(page_title="Financial Planning Calculator")
 
 st.title("Financial Planning Calculator")
