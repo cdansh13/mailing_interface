7c08e3622ae1ae16095b65560b0b14484f53e8b3
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 14:12:35 +0530

    Update and rename calculator.py to c

--- calculator.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("Calculator-Ansh Sharma")
-st.header("This calculator can do Addition")
-a=text_input(int("1st number"))
-b=text_input(int("2nd number"))
-c=st.button("Add")
-if c:
-   st.write(a+b)
