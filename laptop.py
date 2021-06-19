245af961545101ab73dd2406ad1573ac4a20737c
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 23:35:18 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 try:
    os.mkdir("C:\\icomputer")
-except:
-   pass
+except Exception as e:
+   st.error(e)
 st.title("Laptop Desktop info")
 st.subheader("Just download this file and it would be saved in C:\\icomputer")
 down=st.button("Download")
