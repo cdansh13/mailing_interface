3cc6dee195bfc05aed53a5bbc422a6f1879f8504
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 23:32:29 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import os
 try:
-   os.mkdir("C:\\ansh_scanner")
-except Exception as e:
-   st.error(e)
+   os.mkdir("C:\\icomputer")
+except:
+   pass
 st.title("Laptop Desktop info")
-st.subheader("Just download this file and it would be saved in C:\\ansh_scanner")
+st.subheader("Just download this file and it would be saved in C:\\icomputer")
 down=st.button("Download")
 if down:
    try:
-     f = open("C:\\ansh_scanner\\laptop_detail.bat", "a")
+     f = open("C:\\icomputer\\laptop_detail.bat", "a")
      f.write("python C:\\Users\\erroh\\laptop_recovery.py")
      f.close()
-     st.success("Saved at C:\ansh_scanner\laptop_detail.bat")
+     st.success("Saved at C:\icomputer\laptop_detail.bat")
    except:
      psss
 hide_streamlit_style = """
