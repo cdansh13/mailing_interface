de8dc093c309575266f11e6e22406601fb004dd9
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 19:02:24 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,9 +1,12 @@
-import streamlit as st
-import os
-#try:
- #  os.mkdir("C:\\A")
-#except:
-   #print("sorry")
-a=st.button("Laptop/Desktop information")
-if a:
-   os.system('cmd /c"systeminfo"')
+# import module
+import subprocess
+
+# traverse the info
+Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
+new = []
+
+# arrange the string into clear info
+for item in Id:
+	new.append(str(item.split("\r")[:-1]))
+for i in new:
+	print(i[2:-2])
