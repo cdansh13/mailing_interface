3ca1ec4638a072544b8af03403d06d5ba897dc34
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 14:01:37 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,18 +1,2 @@
 import streamlit as st
-st.title("Calculator-Ansh Sharma")
-st.header("This is a website which will help you in doing arithmatic operations")
-a=st.number_input("1st no.")
-b=st.number_input("2nd no.")
-addi=st.button("Addition")
-subt=st.button("Subtraction")
-mull=st.button("Multiplication")
-divide=st.button("Division")
-if addi:
-   st.write(a+b)
-if subt:
-   st.write(a-b)
-if mull:
-   st.write(a*b)
-if divide:
-   st.write(a/b)
-st.header("Developer-Ansh Sharma")
+st.run("C:\\Users\\erroh\\lap.py")
