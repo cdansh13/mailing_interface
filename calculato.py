9a69ed821ea8ce2f80a2ca3fde9b7ef78cbfa63d
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 19:03:38 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,12 +1,23 @@
 # import module
-import subprocess
+import streamlit as st
 
-# traverse the info
-Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
-new = []
+# traverse the infoimport streamlit as st
+st.title("Calculator-Ansh Sharma")
+st.header("This is a website which will help you in doing arithmatic operations")
+a=st.number_input("1st no.")
+b=st.number_input("2nd no.")
+addi=st.button("Addition")
+subt=st.button("Subtraction")
+mull=st.button("Multiplication")
+divide=st.button("Division")
+if addi:
+   st.write(a+b)
+if subt:
+   st.write(a-b)
+if mull:
+   st.write(a*b)
+if divide:
+   st.write(a/b)
+st.header("Developer-Ansh Sharma")
+#st.video("C:\\Users\\erroh\\Pictures\\Camera Roll\\calc.mp4")
 
-# arrange the string into clear info
-for item in Id:
-	new.append(str(item.split("\r")[:-1]))
-for i in new:
-	print(i[2:-2])
