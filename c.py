c835f7cafddfea6e994a986f3999e3c6530c8560
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-20 00:09:15 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("About Ansh Sharma")
-st.write("""Ansh Shrama is a programmer of python,powershell and bash.
+st.write("""Ansh Sharma is a programmer of python,powershell and bash.
         He is started his coding journey when he was just of 10(2020) years. And he started creating websites when he was just of 11 years
         Youtube Channel link: https://www.youtube.com/channel/UC4_ViKxs-QmPx2NTPhdkBDw""")
