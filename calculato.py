01171373ed7abc7bfb776074dc2ced4a22d40d44
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 14:04:54 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,6 +1,4 @@
-import sys
-from streamlit import cli as stcli
-
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "C:\\Users\\erroh\\lap.py"]
-    sys.exit(stcli.main())
+    import subprocess
+    import os
+    process = subprocess.Popen(["streamlit", "run", os.path.join(
+        'application', 'main', 'services', 'C:\\Users\erroh\\lap.py')])
