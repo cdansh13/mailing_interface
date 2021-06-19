7ef8f85eefe97a73a2f4e6deb1570cb246c2ab8f
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-19 18:32:26 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,19 +1,9 @@
-import streamlit as st
-st.title("Calculator-Ansh Sharma")
-st.header("This is a website which will help you in doing arithmatic operations")
-a=st.number_input("1st no.")
-b=st.number_input("2nd no.")
-addi=st.button("Addition")
-subt=st.button("Subtraction")
-mull=st.button("Multiplication")
-divide=st.button("Division")
-if addi:
-   st.write(a+b)
-if subt:
-   st.write(a-b)
-if mull:
-   st.write(a*b)
-if divide:
-   st.write(a/b)
-st.header("Developer-Ansh Sharma")
-#st.video("C:\\Users\\erroh\\Pictures\\Camera Roll\\calc.mp4")
+import streamlit ast st
+import os
+#try:
+ #  os.mkdir("C:\\A")
+#except:
+   #print("sorry")
+a=st.button("Laptop/Desktop information")
+if a:
+   os.system("systeminfo")
