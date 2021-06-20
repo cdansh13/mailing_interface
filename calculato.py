07b959c7a83703fb81fe37f74ef59cda81720726
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-20 12:23:23 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -31,50 +31,6 @@ if choice=="Calculator":
    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 if choice=="BMI calculator":
    # import the streamlit library
-   import streamlit as st
-   st.title('Welcome to BMI Calculator')
-   weight = st.number_input("Enter your weight (in kgs)")
-   status = st.radio('Select your height format: ',
-				        ('cms', 'meters', 'feet'))
-   if(status == 'cms'):
-	   height = st.number_input('Centimeters')
-	
-	   try:
-		   bmi = weight / ((height/100)**2)
-	   except:
-		   st.error("Enter some value of height")
-		
-   elif(status == 'meters'):
-       height = st.number_input('Meters')
-	
-       try:
-	  bmi = weight / (height ** 2)
-       except:
-          st.error("Enter some value of height")
-		
-      else:
-	# take height input in feet
-	       height = st.number_input('Feet')
-	
-	# 1 meter = 3.28
-	       try:
-		       bmi = weight / (((height/3.28))**2)
-	       except:
-		       st.error("Enter some value of height")
-
-# check if the button is pressed or not
-      if(st.button('Calculate BMI')):
-	      st.text("Your BMI Index is {}.".format(bmi))
-	
-	# give the interpretation of BMI index
-	      if(bmi < 16):
-		      st.error("You are Extremely Underweight")
-	      elif(bmi >= 16 and bmi < 18.5):
-		       st.warning("You are Underweight")
-	      elif(bmi >= 18.5 and bmi < 25):
-		        st.success("Healthy")		
-	      elif(bmi >= 25 and bmi < 30):
-		        st.warning("Overweight")
-	      elif(bmi >= 30):
-		        st.error("Extremely Overweight")
+   import os
+   os.system("C:\\Users\\erroh\\Streamlit_folder\\BMI_calculator.py")
 
