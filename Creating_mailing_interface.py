d3144876219d3ec7027fad7b74358962464e4b4f
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-20 09:15:32 +0530

    Update Creating_mailing_interface.py

--- Creating_mailing_interface.py
+++ Creating_mailing_interface.py
@@ -21,3 +21,10 @@ if a:
 # Hereby terminate the session
       smtpobj.quit()
       st.write("sent")
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
