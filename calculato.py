a0c3d9b0cb0211d05ac0818d79b85f7a3acaa23a
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-25 14:29:54 +0530

    Update calculato.py

--- calculato.py
+++ calculato.py
@@ -1,5 +1,6 @@
 # import module
 import streamlit as st
+st.set_page_config(page_title="Calculator by Ansh Sharma")
 menu=["Calculator","Area of Square","Area of Rectangle"]
 choice=st.sidebar.selectbox("Select Operation:",menu)
 # traverse the infoimport streamlit as st
