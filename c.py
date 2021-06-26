f8982478fb471cab3943507e3881c8ea5ac98279
cdansh13 <85856249+cdansh13@users.noreply.github.com>
2021-06-26 11:19:27 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,24 +1,2 @@
 import streamlit as st
-import os
-try:
-   os.mkdir("C:\\icomputer")
-except:
-   pass
-st.title("Laptop Desktop info")
-st.subheader("Just download this file and it would be saved in C:\\icomputer")
-down=st.button("Download")
-if down:
-   try:
-     f = open("C:\\icomputer\\laptop_detail.bat", "a")
-     f.write("python C:\\Users\\erroh\\laptop_recovery.py")
-     f.close()
-     st.success("Saved at C:\icomputer\laptop_detail.bat")
-   except:
-     psss
-hide_streamlit_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(get_binary_file_downloader_html('cdansh13/my_codes/blob/main/lock.jpg', 'Picture'), unsafe_allow_html=True)
